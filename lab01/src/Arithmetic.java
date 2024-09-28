01112d975feeeb48c9c8176253fc86489dbd17a7
whitney-house <116066507+whitney-house@users.noreply.github.com>
2024-09-28 14:49:13 +0200

    finish lab01

--- lab01/src/Arithmetic.java
+++ lab01/src/Arithmetic.java
@@ -1,5 +1,10 @@
 import edu.princeton.cs.algs4.StdIn;
 
+/**
+ * Course: CS 61b
+ * Author: Weiting Wang
+ */
+
 /** Simple Arithmetic Class.
  * @author Josh Hug
  * */
@@ -20,7 +25,7 @@ public class Arithmetic {
      * @return Sum of a and b
      * */
     public static int sum(int a, int b) {
-        return a * b;
+        return a + b;
     }
 
     public static void main(String[] args) {
